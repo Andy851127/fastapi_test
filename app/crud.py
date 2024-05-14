c1acc0907fa844425144a4423cb3b72e077c288e
Andy851127 <=>
2024-05-14 21:14:16 +0800

    [perf] modify crud to class & main.py import Blog

--- app/crud.py
+++ app/crud.py
@@ -1,31 +1,40 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-def get_blog(db: Session, blog_id: int):
-    return db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+from sqlalchemy.orm import Session
+from . import models, schemas
 
-def get_blogs(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Blog).offset(skip).limit(limit).all()
+class Blog:
+    @staticmethod
+    def get_blog(db: Session, blog_id: int):
+        return db.query(models.Blog).filter(models.Blog.id == blog_id).first()
 
-def create_blog(db: Session, blog: schemas.BlogCreate):
-    db_blog = models.Blog(**blog.dict())
-    db.add(db_blog)
-    db.commit()
-    db.refresh(db_blog)
-    return db_blog
+    @staticmethod
+    def get_blogs(db: Session, skip: int = 0, limit: int = 10):
+        return db.query(models.Blog).offset(skip).limit(limit).all()
 
-def update_blog(db: Session, blog_id: int, blog: schemas.BlogCreate):
-    db_blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
-    if db_blog:
-        for key, value in blog.dict().items():
-            setattr(db_blog, key, value)
+    @staticmethod
+    def create_blog(db: Session, blog: schemas.BlogCreate):
+        db_blog = models.Blog(**blog.dict())
+        db.add(db_blog)
         db.commit()
         db.refresh(db_blog)
-    return db_blog
+        return db_blog
 
-def delete_blog(db: Session, blog_id: int):
-    db_blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
-    if db_blog:
-        db.delete(db_blog)
-        db.commit()
-    return db_blog
+    @staticmethod
+    def update_blog(db: Session, blog_id: int, blog: schemas.BlogCreate):
+        db_blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+        if db_blog:
+            for key, value in blog.dict().items():
+                setattr(db_blog, key, value)
+            db.commit()
+            db.refresh(db_blog)
+        return db_blog
+
+    @staticmethod
+    def delete_blog(db: Session, blog_id: int):
+        db_blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
+        if db_blog:
+            db.delete(db_blog)
+            db.commit()
+        return db_blog
--- app/main.py
+++ app/main.py
@@ -8,30 +8,30 @@ models.Base.metadata.create_all(bind=database.engine)
 
 @app.post("/blogs/", response_model=schemas.Blog)
 def create_blog(blog: schemas.BlogCreate, db: Session = Depends(database.get_db)):
-    return crud.create_blog(db=db, blog=blog)
+    return crud.Blog.create_blog(db=db, blog=blog)
 
 @app.get("/blogs/", response_model=list[schemas.Blog])
 def read_blogs(skip: int = 0, limit: int = 10, db: Session = Depends(database.get_db)):
-    blogs = crud.get_blogs(db, skip=skip, limit=limit)
+    blogs = crud.Blog.get_blogs(db, skip=skip, limit=limit)
     return blogs
 
 @app.get("/blogs/{blog_id}", response_model=schemas.Blog)
-def read_blog(blog_id: int, db: Session = Depends(database.get_db)):
-    db_blog = crud.get_blog(db, blog_id=blog_id)
+def get_blog(blog_id: int, db: Session = Depends(database.get_db)):
+    db_blog = crud.Blog.get_blog(db, blog_id=blog_id)
     if db_blog is None:
         raise HTTPException(status_code=404, detail="Blog not found")
     return db_blog
 
 @app.put("/blogs/{blog_id}", response_model=schemas.Blog)
 def update_blog(blog_id: int, blog: schemas.BlogCreate, db: Session = Depends(database.get_db)):
-    db_blog = crud.update_blog(db, blog_id=blog_id, blog=blog)
+    db_blog = crud.Blog.update_blog(db, blog_id=blog_id, blog=blog)
     if db_blog is None:
         raise HTTPException(status_code=404, detail="Blog not found")
     return db_blog
 
 @app.delete("/blogs/{blog_id}", response_model=schemas.Blog)
 def delete_blog(blog_id: int, db: Session = Depends(database.get_db)):
-    db_blog = crud.delete_blog(db, blog_id=blog_id)
+    db_blog = crud.Blog.delete_blog(db, blog_id=blog_id)
     if db_blog is None:
         raise HTTPException(status_code=404, detail="Blog not found")
     return db_blog
